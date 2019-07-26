ffc2105b543934ae438c86896fa97030918141f0
Alexandra <alexandra.schachtner@gmail.com>
2019-07-26 04:16:57 -0500

    hope this works

---
+++ Volunteer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LastChanceCsharp
+{
+    public class Volunteer
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public string Phone { get; set; }
+        public string Licence { get; set; }
+
+        public string Print() { return (First + " " + Last + " " + Phone + " " + Licence); }
+        public string Convert() { return First + "," + Last + "," + Phone + "," + Licence; }
+
+    }
+}
